e6c6e699d5aaf92fdf90842002255a2c7927850c
Argo Roots <argo.roots@adm.ee>
2022-02-17 19:23:02 +0200

    Add sample schema

--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,11 +1,17 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const port = process.env.PORT || 4000
+const schemaFilePath = path.join(__dirname, './schema.gql')
+const schemaFile = fs.readFileSync(schemaFilePath, { encoding: 'utf8', flag: 'r' })
+
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-  schema: MyGraphQLSchema,
+  schema: buildSchema(schemaFile),
   graphiql: true
 }))
 
