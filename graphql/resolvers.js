c13ee31748e88c09453177682f8b00ca85a5fd16
Argo Roots <argo.roots@adm.ee>
2022-02-18 03:16:17 +0200

    Add mutations

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,28 +6,68 @@ function authors () {
   return allAuthors
 }
 
-function author (_, { id }) {
-  return allAuthors.find(a => a.id === parseInt(id))
+function author (obj, args) {
+  return allAuthors.find(a => a.id === parseInt(args.id))
 }
 
 function posts () {
   return allPosts
 }
 
-function post (_, { id }) {
-  return allPosts.find(p => p.id === parseInt(id))
+function post (obj, args) {
+  return allPosts.find(p => p.id === parseInt(args.id))
 }
 
-function authorPosts ({ id }) {
-  return allPosts.filter(p => p.author_id === parseInt(id))
+function submitPost (obj, args) {
+  const author = allAuthors.find(a => a.id === parseInt(args.author_id))
+
+  if (!author) {
+    throw new Error(`Couldn't find author with id ${args.author_id}`)
+  }
+
+  const post = {
+    id: allPosts.length + 1,
+    author_id: author.id,
+    created_at: new Date().toISOString(),
+    title: args.title,
+    body: args.body
+  }
+
+  allPosts.push(post)
+
+  return post
 }
 
-function postAuthor ({ author_id }) {
-  return allAuthors.find(a => a.id === parseInt(author_id))
+function submitComment (obj, args) {
+  const post = allPosts.find(a => a.id === parseInt(args.post_id))
+
+  if (!post) {
+    throw new Error(`Couldn't find post with id ${args.post_id}`)
+  }
+
+  const comment = {
+    id: allComments.length + 1,
+    post_id: post.id,
+    created_at: new Date().toISOString(),
+    author: args.author,
+    body: args.body
+  }
+
+  allComments.push(comment)
+
+  return comment
 }
 
-function postComments ({ id }) {
-  return allComments.filter(c => c.post_id === parseInt(id))
+function authorPosts (author) {
+  return allPosts.filter(p => p.author_id === parseInt(author.id))
+}
+
+function postAuthor (post) {
+  return allAuthors.find(a => a.id === parseInt(post.author_id))
+}
+
+function postComments (post) {
+  return allComments.filter(c => c.post_id === parseInt(post.id))
 }
 
 module.exports = {
@@ -36,6 +76,10 @@ module.exports = {
     author,
     posts,
     post
+  },
+  Mutation: {
+    submitPost,
+    submitComment
   },
   Author: {
     posts: authorPosts
