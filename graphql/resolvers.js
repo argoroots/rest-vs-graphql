6b65a581dfd7ddf827d9b6d07080eea08daed423
Argo Roots <argo.roots@adm.ee>
2022-02-18 02:40:48 +0200

    Refactor resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,18 +2,46 @@ const allAuthors = require('../data/authors.json')
 const allPosts = require('../data/posts.json')
 const allComments = require('../data/comments.json')
 
+function authors () {
+  return allAuthors
+}
+
+function author (_, { id }) {
+  return allAuthors.find(a => a.id === parseInt(id))
+}
+
+function posts () {
+  return allPosts
+}
+
+function post (_, { id }) {
+  return allPosts.find(p => p.id === parseInt(id))
+}
+
+function authorPosts ({ id }) {
+  return allPosts.filter(p => p.author_id === parseInt(id))
+}
+
+function postAuthor ({ author_id }) {
+  return allAuthors.find(a => a.id === parseInt(author_id))
+}
+
+function postComments ({ id }) {
+  return allComments.filter(c => c.post_id === parseInt(id))
+}
+
 module.exports = {
   Query: {
-    authors: () => allAuthors,
-    author: (_, { id }) => allAuthors.find(a => a.id === parseInt(id)),
-    posts: () => allPosts,
-    post: (_, { id }) => allPosts.find(p => p.id === parseInt(id))
+    authors,
+    author,
+    posts,
+    post
   },
   Author: {
-    posts: author => allPosts.filter(p => p.author_id === parseInt(author.id))
+    posts: authorPosts
   },
   Post: {
-    author: post => allAuthors.find(a => a.id === parseInt(post.author_id)),
-    comments: post => allComments.filter(c => c.post_id === parseInt(post.id))
+    author: postAuthor,
+    comments: postComments
   }
 }
